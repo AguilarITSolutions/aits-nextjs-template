ab778ff1c3eedeeb41ddd98a7b0d1eba0a158ab2
Francis <francis.aguilar11@gmail.com>
2024-02-08 15:35:37 +0800

    lint & prettier with new config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-import withBundleAnalyzer from '@next/bundle-analyzer'
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
+  enabled: process.env.ANALYZE === "true",
+});
 
 const ContentSecurityPolicy = `
   default-src 'self';
@@ -13,79 +13,79 @@ const ContentSecurityPolicy = `
   connect-src *;
   font-src 'self';
   frame-src giscus.app
-`
+`;
 
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
-    key: 'X-Frame-Options',
-    value: 'DENY',
+    key: "X-Frame-Options",
+    value: "DENY",
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
   {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
+    key: "X-Content-Type-Options",
+    value: "nosniff",
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
   {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
   {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains',
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains",
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
   {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
   },
-]
+];
 /** @type {import('next').NextConfig} */
 const nextConfig = () => {
-  const plugins = [withBundleAnalyzer]
+  const plugins = [bundleAnalyzer];
   return plugins.reduce((acc, next) => next(acc), {
     reactStrictMode: true,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
     eslint: {
-      dirs: ['app', 'components', 'layouts', 'scripts'],
+      dirs: ["app", "components", "layouts", "scripts"],
     },
-    images: {
-      remotePatterns: [
-        {
-          // protocol: 'https',
-          // hostname: 'picsum.photos',
-        },
-      ],
-    },
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       // protocol: 'https',
+    //       // hostname: 'picsum.photos',
+    //     },
+    //   ],
+    // },
     async headers() {
       return [
         {
-          source: '/(.*)',
+          source: "/(.*)",
           headers: securityHeaders,
         },
-      ]
+      ];
     },
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
+        use: ["@svgr/webpack"],
+      });
 
-      return config
+      return config;
     },
-  })
-}
+  });
+};
 
-export default nextConfig
+export default nextConfig;
